30c110624965c361fa4bda3591f6698835f3521a
UBONGO2000 <gadyubongo@gmail.com>
2024-09-30 15:09:37 +0200

    creation de l'entite user (user1)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,8 +11,38 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        echo "<h1>Bienvenue sur ma home page </h1>";
+        return $this->render('home/index.html.twig', [
+                'controller_name' => 'NTCHANGA',
+                'controller_prenom' => 'Georges',
+                'controller_adresse' => '3 rue du louve',
+                'controller_mail' => 'gadyber6@gmail.com',
+                'controller_path' => 'https://www.php.net/'
+        ]);
+    }
+
+    #[Route('/test', name: 'app_test')]
+    public function test(): Response
+    {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
+
+    #[Route('/profil', name: 'app_profil')]
+    public function profil(): Response
+        
+    {   echo "<h1>Bienvenue sur ma PROFIL-PAGE </h1>";
+        return $this->render('home/index.html.twig', $tableau = [
+                'controller_name' => 'NTCHANGA',
+                'controller_prenom' => 'Georges',
+                'controller_adresse' => '3 rue du louve',
+                'controller_mail' => 'gadyber6@gmail.com',
+                'controller_path' => 'https://www.php.net/'
+            
+        ]);
+    }
+
+    
+
 }
